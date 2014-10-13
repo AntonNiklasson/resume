ad89d9b7de748b450932a34f8a6adeeadbe65ec2
Anton Niklasson <niklasson.anton@gmail.com>
2014-10-13 20:33:57 +0200

    Adedd skills section

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var html2pdf = require('gulp-html2pdf');
 
-gulp.task('default', ['sass', 'pdf'], function() {
-	gulp.watch(['css/sass/**/*.scss'], ['sass', 'pdf']);
-	gulp.watch(['index.html'], ['pdf']);
+gulp.task('default', ['sass'], function() {
+	gulp.watch(['css/sass/**/*.scss'], ['sass']);
 });
 
 gulp.task('sass', function() {
 	return gulp.src(['css/sass/style.scss'])
 		.pipe(sass())
 		.pipe(gulp.dest('./css'));
-})
-
-gulp.task('pdf', function() {
-	return gulp.src(['index.html'])
-		.pipe(html2pdf())
-		.pipe(gulp.dest('./'));
 });
