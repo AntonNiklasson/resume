3e3c87960ad0af67b9b07ac38058762394232343
Anton Niklasson <niklasson.anton@gmail.com>
2014-10-26 12:54:52 +0100

    Added some more content and scss partials.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
 
 gulp.task('default', ['sass'], function() {
+	livereload.listen();
 	gulp.watch(['css/sass/**/*.scss'], ['sass']);
+	gulp.watch(['*.html'], ['livereload']);
+});
+
+gulp.task('livereload', function() {
+	return gulp.src(['*.html', 'css/**/*.css'])
+		.pipe(livereload());
 });
 
 gulp.task('sass', function() {
 	return gulp.src(['css/sass/style.scss'])
+		.pipe(plumber())
 		.pipe(sass())
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./css'))
+		.pipe(livereload());
 });
