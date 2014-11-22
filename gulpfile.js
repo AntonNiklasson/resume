14166f8601930279a7b16a94384fba2b19e4aa83
Anton Niklasson <niklasson.anton@gmail.com>
2014-11-22 09:00:51 +0100

    Added browsersync and a print button

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var plumber = require('gulp-plumber');
-var livereload = require('gulp-livereload');
+var browsersync = require('browser-sync');
 
-gulp.task('default', ['sass'], function() {
-	livereload.listen();
+gulp.task('default', ['sass', 'browsersync'], function() {
 	gulp.watch(['css/sass/**/*.scss'], ['sass']);
-	gulp.watch(['*.html'], ['livereload']);
-});
-
-gulp.task('livereload', function() {
-	return gulp.src(['*.html', 'css/**/*.css'])
-		.pipe(livereload());
 });
 
 gulp.task('sass', function() {
 	return gulp.src(['css/sass/style.scss'])
 		.pipe(plumber())
 		.pipe(sass())
-		.pipe(gulp.dest('./css'))
-		.pipe(livereload());
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('browsersync', function() {
+    browsersync({
+        server: {
+            baseDir: './'
+        }
+    });
 });
