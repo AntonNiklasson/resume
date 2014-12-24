4291a91c47fa11f4fa98bdbc3d1bdbd027477f5b
Anton Niklasson <niklasson.anton@gmail.com>
2014-12-24 15:44:00 +0100

    extracted content to data.json and templating with hbs from now on.

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var plumber = require('gulp-plumber');
 var webserver = require('gulp-webserver');
+var fs = require('fs');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
 
-gulp.task('default', ['sass', 'webserver'], function() {
-	gulp.watch(['css/sass/**/*.scss'], ['sass']);
+gulp.task('default', ['template', 'sass', 'webserver'], function() {
+	gulp.watch(['./css/sass/**/*.scss'], ['sass']);
+	gulp.watch(['./index.hbs', './data.json'], ['template']);
 });
 
 gulp.task('webserver', function() {
@@ -21,4 +25,14 @@ gulp.task('sass', function() {
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('template', function() {
+	var file = fs.readFileSync('./data.json', 'utf8');
+	var data = JSON.parse(file);
+
+	return gulp.src('index.hbs')
+		.pipe(handlebars(data))
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('./'));
 });
