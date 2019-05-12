e4d3116fbe9f54bcb0b0547c4c5fb439a26afb90
Anton Niklasson <niklasson.anton@gmail.com>
2019-05-12 11:32:28 +0200

    fix ports in prod

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,11 @@
 #! /usr/bin/env node
 
-const exec = require("ssh-exec");
+const exec = require("ssh-exec")
 
-const user = "root";
-const host = "95.85.43.196";
-const path = "/var/www/cv.antonniklasson.se";
-const branch = "2.0";
+const user = "root"
+const host = "95.85.43.196"
+const path = "/var/www/cv.antonniklasson.se"
+const branch = "2.0"
 
 const script = `
 	cd ${path};
@@ -18,8 +18,8 @@ const script = `
 	docker stop resume-container || true;
 	docker rm resume-container || true;
 	docker run --rm -d -p 8001:80 --name resume-container resume;
-`;
+`
 
-(async () => {
-  await exec(script, `${user}@${host}`).pipe(process.stdout);
-})();
+;(async () => {
+  await exec(script, `${user}@${host}`).pipe(process.stdout)
+})()
